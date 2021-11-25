5a9cd9b1a82d1713f18c9f7e0d84944863e61c69
vitor-mat <vitormateusalves123@gmail.com>
2021-11-25 16:50:10 -0300

    painel de edicao do timer e mostrado e escondido de acordo os botoes de controle

--- script.js
+++ script.js
@@ -24,3 +24,33 @@ function createInputs(){
 }
 
 createInputs()
+
+document.getElementById("btn-edit").addEventListener("click", () => {
+    const blackScreen = document.getElementById("black-screen")
+
+    const editPainel = document.getElementById("edit-painel")
+
+    blackScreen.classList.add("show-black-painel")
+
+    editPainel.classList.add("show-edit-painel")
+})
+
+document.getElementById("edit-painel-controls-cancel-btn").addEventListener("click", ()=>{
+    const blackScreen = document.getElementById("black-screen")
+
+    const editPainel = document.getElementById("edit-painel")
+
+    blackScreen.classList.remove("show-black-painel")
+
+    editPainel.classList.remove("show-edit-painel")
+})
+
+document.getElementById("edit-painel-controls-start-btn").addEventListener("click", ()=>{
+    const blackScreen = document.getElementById("black-screen")
+
+    const editPainel = document.getElementById("edit-painel")
+
+    blackScreen.classList.remove("show-black-painel")
+
+    editPainel.classList.remove("show-edit-painel")
+})
